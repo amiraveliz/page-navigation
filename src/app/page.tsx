ab0044fa3199607fcc10f8f33bf0386ff753355d
Amira Veliz <amira.veliz@gmail.com>
2026-09-21 12:28:51 +0200

    upgrading nextjs version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex-1 flex items-center text-2xl md:text-3xl my-2">
+    <div className="flex-1 flex items-center text-2xl md:text-3xl my-2 text-white">
       Welcome to Form Nagivation challengue
     </div>
   );
